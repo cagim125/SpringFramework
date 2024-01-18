69f562d555c741d2321108fcb7207f1fbc89667f
cagim125 <cagim30@gmail.com>
2024-01-18 11:37:42 +0900

    EMS 수정

--- ch04_pjt_01/src/main/java/ch04_pjt_01/ems/member/service/EMSInformationService.java
+++ ch04_pjt_01/src/main/java/ch04_pjt_01/ems/member/service/EMSInformationService.java
@@ -2,4 +2,20 @@ package ch04_pjt_01.ems.member.service;
 
 public class EMSInformationService {
 
+    private String info;
+    private String copyright;
+    private String version;
+    private String sYear;
+    private String sMonth;
+    private String sDay;
+    private String eYear;
+    private String eMonth;
+    private String eDay;
+    private List<String> developer;
+    private Map<String, String> administrator;
+    private Map<String, DBConnectionInfo> dbInfo;
+
+    public void printEMSInformation() {
+        System.out.println("EMS INFORMATION START--------")
+    }
 }
